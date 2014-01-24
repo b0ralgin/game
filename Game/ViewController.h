0623956ad84fd36d4eb91066e6f463459a4575f5
Akira Yamaoka <akira@ip-192-168-0-106.is74.loc>
2014-01-24 22:56:30 +0600

    first

--- Game/ViewController.h
+++ Game/ViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <SpriteKit/SpriteKit.h>
+#import "GameScene.h"
 
 @interface ViewController : UIViewController
 
--- GameScene.m
+++ GameScene.m
@@ -19,6 +19,30 @@
     return self;
 }
 -(void)initRoomBound{
-    SKSpriteNode *floor = [SKSpriteNode spriteNodeWithColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0] size:CGSizeMake(self.size.width, 2)];
+     SKSpriteNode *floor = [SKSpriteNode spriteNodeWithColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:10] size:CGSizeMake(self.size.width, 1)];
+    floor.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:floor.size];
+    floor.physicsBody.dynamic = NO;
+    floor.position = CGPointMake(self.size.width/2, 0);
+    [self addChild:floor];
+    
+    SKSpriteNode *ceiling = [SKSpriteNode spriteNodeWithColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:10] size:CGSizeMake(self.size.width, 1)];
+    ceiling.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:floor.size];
+    ceiling.physicsBody.dynamic = NO;
+    ceiling.position = CGPointMake(self.size.width/2, self.size.height);
+    [self addChild:ceiling];
+    
+    SKSpriteNode *leftWall = [SKSpriteNode spriteNodeWithColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:10] size:CGSizeMake(1, self.size.height)];
+    leftWall.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:floor.size];
+    leftWall.physicsBody.dynamic = NO;
+    leftWall.position = CGPointMake(0,self.size.height/2);
+    [self addChild:leftWall];
+    
+    SKSpriteNode *rightWall = [SKSpriteNode spriteNodeWithColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:10] size:CGSizeMake(1, self.size.height)];
+    rightWall.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:floor.size];
+    rightWall.physicsBody.dynamic = NO;
+    rightWall.position = CGPointMake(self.size.width,self.size.height/2);
+    [self addChild:rightWall];
+    
+    
 }
 @end
