8c5b7579d6341eb0d3e7e774e1712d1b6bd02776
Enso <Enso@ip-192-168-0-107.is74.loc>
2014-01-25 03:17:47 +0600

    added masks

--- Game/Mask.h
+++ Game/Mask.h
@@ -13,4 +13,20 @@ static const uint32_t CandyColisionBM = 0x0 << 0;
 static const uint32_t NonTouchTrapColisionBM = 0x0 << 0;
 static const uint32_t EnemyColBM = 0b;
 static const uint32_t ColBM = 0b0001;
+
+
+
+static const uint32_t kColisionRoom = 0b00011;
+static const uint32_t kCollisionBox = 0b00101;
+static const uint32_t kColisionTrap = 0b00111;
+static const uint32_t kColisionEnemy = 0b00111;
+static const uint32_t kColisionGirl = 0b11111;
+
+static const uint32_t kContactFire = 0b00011;
+static const uint32_t kContactCandy=0b11111;
+static const uint32_t kContactTrap = 0b10001;
+static const uint32_t kContactEnemy = 0b10001;
+static const uint32_t kContactGirl = 0b11111;
+
+
 #endif
