2654d087938e04eefa912733ec6673381244cbf2
Enso <Enso@ip-192-168-0-107.is74.loc>
2014-01-24 22:45:07 +0600

    add Class Enemy

---
+++ Game/Enemy.h
@@ -0,0 +1,13 @@
+//
+//  Enemy.h
+//  Game
+//
+//  Created by Enso on 24.01.14.
+//  Copyright (c) 2014 AppBit. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface Enemy : SKSpriteNode
+
+@end
---
+++ Game/Enemy.m
@@ -0,0 +1,13 @@
+//
+//  Enemy.m
+//  Game
+//
+//  Created by Enso on 24.01.14.
+//  Copyright (c) 2014 AppBit. All rights reserved.
+//
+
+#import "Enemy.h"
+
+@implementation Enemy
+
+@end
