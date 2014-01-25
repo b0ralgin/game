522e9d4d34dc6e99e5ef1ccfea8c9303a08046d3
Akira Yamaoka <akira@ip-192-168-0-102.is74.loc>
2014-01-25 09:15:41 +0600

    contact

---
+++ UIView+SimplePhysicContactDelegate.h
@@ -0,0 +1,13 @@
+//
+//  UIView+SimplePhysicContactDelegate.h
+//  Game
+//
+//  Created by Akira Yamaoka on 25.01.14.
+//  Copyright (c) 2014 AppBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (SimplePhysicContactDelegate)
+
+@end
